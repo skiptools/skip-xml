34a09f8fe411ae1efc5a3635e5766f300e929878
marcprux <mwp1@cornell.edu>
2023-09-06 09:09:49 -0400

    Modernize package

--- Package.swift
+++ Package.swift
@@ -7,28 +7,15 @@ let package = Package(
     platforms: [.iOS(.v16), .macOS(.v13), .tvOS(.v16), .watchOS(.v9), .macCatalyst(.v16)],
     products: [
     .library(name: "SkipXML", targets: ["SkipXML"]),
-    .library(name: "SkipXMLKt", targets: ["SkipXMLKt"]),
     ],
     dependencies: [
-        .package(url: "https://source.skip.tools/skip.git", from: "0.0.0"),
-        .package(url: "https://source.skip.tools/skip-unit.git", from: "0.0.0"),
-        .package(url: "https://source.skip.tools/skip-lib.git", from: "0.0.0"),
-        .package(url: "https://source.skip.tools/skip-foundation.git", from: "0.0.0"),
+        .package(url: "https://source.skip.tools/skip.git", from: "0.6.39"),
+        .package(url: "https://source.skip.tools/skip-foundation.git", from: "0.1.9"),
     ],
     targets: [
-    .target(name: "SkipXML", plugins: [.plugin(name: "skippy", package: "skip")]),
-    .target(name: "SkipXMLKt", dependencies: [
-        "SkipXML",
-        .product(name: "SkipUnitKt", package: "skip-unit"),
-        .product(name: "SkipLibKt", package: "skip-lib"),
-        .product(name: "SkipFoundationKt", package: "skip-foundation"),
-    ], resources: [.process("Skip")], plugins: [.plugin(name: "transpile", package: "skip")]),
+    .target(name: "SkipXML", dependencies: [.product(name: "SkipFoundation", package: "skip-foundation", condition: .when(platforms: [.macOS]))], plugins: [.plugin(name: "skipstone", package: "skip")]),
     .testTarget(name: "SkipXMLTests", dependencies: [
         "SkipXML"
-    ], plugins: [.plugin(name: "skippy", package: "skip")]),
-    .testTarget(name: "SkipXMLKtTests", dependencies: [
-        "SkipXMLKt",
-        .product(name: "SkipUnit", package: "skip-unit"),
-    ], resources: [.process("Skip")], plugins: [.plugin(name: "transpile", package: "skip")]),
+    ], plugins: [.plugin(name: "skipstone", package: "skip")]),
     ]
 )
--- Sources/SkipXMLKt/SkipXMLBundle.swift
+++
@@ -1,4 +0,0 @@
-import Foundation
-public extension Bundle {
-    static let SkipXMLBundle = Bundle.module
-}
