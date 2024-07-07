14cc5f636a2e29a711b2fde9e739b24950560533
marcprux <mwp1@cornell.edu>
2024-07-07 10:35:59 -0400

    Dependency bump

--- Package.swift
+++ Package.swift
@@ -6,11 +6,11 @@ let package = Package(
     defaultLocalization: "en",
     platforms: [.iOS(.v16), .macOS(.v13), .tvOS(.v16), .watchOS(.v9), .macCatalyst(.v16)],
     products: [
-        .library(name: "SkipXML", targets: ["SkipXML"]),
+        .library(name: "SkipXML", type: .dynamic, targets: ["SkipXML"]),
     ],
     dependencies: [
-        .package(url: "https://source.skip.tools/skip.git", from: "0.8.45"),
-        .package(url: "https://source.skip.tools/skip-foundation.git", from: "0.6.9"),
+        .package(url: "https://source.skip.tools/skip.git", from: "0.9.4"),
+        .package(url: "https://source.skip.tools/skip-foundation.git", from: "0.7.0"),
     ],
     targets: [
         .target(name: "SkipXML", dependencies: [
